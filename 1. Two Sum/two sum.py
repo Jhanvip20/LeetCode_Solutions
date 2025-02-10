939b7807575f2e426267b28f055ad5a12720b012
Jhanvi Patel <117141234+Jhanvip20@users.noreply.github.com>
2025-02-10 18:10:39 -0500

    Update two sum.py

--- 1. Two Sum/two sum.py
+++ 1. Two Sum/two sum.py
@@ -1,1 +1,9 @@
 
+class Solution:
+    def twoSum(self, nums: List[int], target: int) -> List[int]:
+        num_dict = {}  # Dictionary to store number and its index
+        for i, num in enumerate(nums):
+            complement = target - num
+            if complement in num_dict:
+                return [num_dict[complement], i]  # Return the indices of the two numbers
+            num_dict[num] = i  # Add the current number and its index to the dictionary
