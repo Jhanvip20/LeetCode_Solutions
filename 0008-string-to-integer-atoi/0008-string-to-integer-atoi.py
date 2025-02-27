8416389a914b5362694955c1235bbd3ebbd51877
Jhanvi Patel <117141234+Jhanvip20@users.noreply.github.com>
2025-02-26 23:08:38 -0500

    Time: 5 ms (7.79%), Space: 17.8 MB (77.39%) - LeetHub

--- 0008-string-to-integer-atoi/0008-string-to-integer-atoi.py
+++ 0008-string-to-integer-atoi/0008-string-to-integer-atoi.py
@@ -1,24 +1,24 @@
 class Solution:
     def myAtoi(self, s: str) -> int:
         s = s.lstrip() # Ignore Leading Whitespace
-        if not s:
+        if not s: #check for empty string
             return 0
         
-        sign, index, result = 1, 0, 0
-        if s[index] in '+-':
+        sign, index, result = 1, 0, 0         # Initialize sign as positive (1) and start index and result
+        if s[index] in '+-':    #Check for a sign ('+' or '-') at the beginning
             sign = -1 if s[index] == '-' else 1
             index += 1
-        
+        #Process digits in the string
         while index < len(s) and s[index].isdigit():
             result = result * 10 + int(s[index])
             index += 1
-
+            #Handle overflow/underflow within 32-bit integer range
             if sign * result > (2**31 - 1):
                 return 2**31 - 1 
             if sign * result <= -2**31:
                 return -2**31
                
-        return result * sign
+        return result * sign #Return the final result with the correct sign
 
         
 
