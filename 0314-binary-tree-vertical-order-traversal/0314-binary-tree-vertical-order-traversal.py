33edddfe98546cc2f037fe3f2f61d1aa9c4c51d8
Jhanvi Patel <117141234+Jhanvip20@users.noreply.github.com>
2025-08-01 15:52:05 -0400

    Time: 0 ms (100%), Space: 17.9 MB (21.91%) - LeetHub

--- 0314-binary-tree-vertical-order-traversal/0314-binary-tree-vertical-order-traversal.py
+++ 0314-binary-tree-vertical-order-traversal/0314-binary-tree-vertical-order-traversal.py
@@ -1,19 +1,23 @@
-# Definition for a binary tree node.
-# class TreeNode:
-#     def __init__(self, val=0, left=None, right=None):
-#         self.val = val
-#         self.left = left
-#         self.right = right
+from collections import defaultdict, deque
+
 class Solution:
-    def verticalOrder(self, root: Optional[TreeNode]) -> List[List[int]]:
-        # Dictionary to store nodes grouped by their vertical column index
-        cols = collections.defaultdict(list)
-        queue = collections.deque([(root, 0)]) # Initialize queue for BFS traversal, storing (node, column index)
+    def verticalOrder(self, root: TreeNode) -> List[List[int]]:
+        if not root:
+            return []
+        
+        columns = defaultdict(list)
+        queue = deque([(root, 0)])
+        columns[0] = [root.val]
+        
         while queue:
-            node, i = queue.popleft() # Dequeue element
-            if node:
-                cols[i].append(node.val) # Store node value in the corresponding column index
-                # Add left child to queue with column index decreased by 1
-                # Add right child to queue with column index increased by 1
-                queue += (node.left, i - 1), (node.right, i + 1) 
-        return [cols[i] for i in sorted(cols)] # Return a list of node values in sorted column order
+            node, col = queue.popleft()
+            
+            if node.left:
+                queue.append((node.left, col - 1))
+                columns[col - 1].append(node.left.val)
+                
+            if node.right:
+                queue.append((node.right, col + 1))
+                columns[col + 1].append(node.right.val)
+                
+        return [columns[x] for x in sorted(columns.keys())]
